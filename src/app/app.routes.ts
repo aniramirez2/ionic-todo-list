ac9345d2786314e55a1af7adc1fca546af143593
Ana <ramirezarboledaana@gmail.com>
2024-11-17 13:03:44 -0500

    fix/routes-behavior

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ export const routes: Routes = [
     path: '',
     component: LayoutComponent, // El layout principal
     children: [
+      { path: '', redirectTo: '/home', pathMatch: 'full' },
       { path: 'home', loadComponent: () => import('./pages/home/home.page').then(m => m.HomePage) },
       { path: 'categories', loadComponent: () => import('./pages/categories/categories.page').then(m => m.CategoriesPage) },
     ],
